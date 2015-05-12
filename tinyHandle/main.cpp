d440c80e7cb792f9ade062f24c9ba00a9f29aae9
swjtuLzr <liuzhaorui1@163.com>
2015-05-12 13:26:16 +0800

    test explicit

--- tinyHandle/main.cpp
+++ tinyHandle/main.cpp
@@ -5,7 +5,8 @@ class Base
 {
     public:
         Base(int n) : val(n) {}
-        int val; 
+        int val;
+        explicit Base(const Base &){}; 
         virtual void out()
         {
             std::cout << "base:"<<val<<std::endl;
@@ -22,9 +23,15 @@ class Child : public Base
         }
 };
 
+void out(Base tt)
+{
+    std::cout << tt.val << std::endl;
+}
+
 int main()
 {
     //一个生成的对象指针只能交给一个 handle类管理
+    
     Base *item_test1 = new Base(1);
     Child *item_test2 = new Child(2);
 
@@ -36,6 +43,17 @@ int main()
   
     Handle<Base> test3 = test1;
     Handle<Base> test4;
-    test4 = test1; 
+    test4 = test1;
+
+    //test explicit
+    Base base1(1);
+
+    //error explicit Base
+    //explicit = 初始化 不能运行 同理函数参数传递时候调用的
+    //拷贝构造函数为explicit 不能进行转化。所以函数此处调用
+    //也是错误
+    //Base base2 = base1;
+    //out(base1);
+    
     return 0;
 }
